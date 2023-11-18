89cb3f4e48e5f5ac2a1e825ac841af27a536353e
marcustakeyasu <116593545+marcustakeyasu@users.noreply.github.com>
2023-11-18 02:17:08 +0000

    Add files via upload

--- Calculadora.java
+++ Calculadora.java
@@ -1,7 +1,24 @@
+/**
+ * A classe Calculadora representa uma calculadora simples que realiza operações matemáticas
+ * utilizando objetos que implementam a interface Operacao.
+ * 
+ * @author Marcus Takeyasu
+ * @version 1.01
+ * @since 17/11/2023
+ */
 package projeto_calc;
 
 public class Calculadora {
-	 public int calcular(Operacao operacao, int num1, int num2) {
-	        return operacao.executar(num1, num2);
-	    }
+    
+    /**
+     * Método para realizar uma operação matemática utilizando um objeto que implementa a interface Operacao.
+     * 
+     * @param operacao Um objeto que implementa a interface Operacao (por exemplo, Soma, Subtracao, etc.).
+     * @param num1 O primeiro número da operação.
+     * @param num2 O segundo número da operação.
+     * @return O resultado da operação matemática.
+     */
+    public int calcular(Operacao operacao, int num1, int num2) {
+        return operacao.executar(num1, num2);
+    }
 }
