96ed26479e6215864e9957abd625fccf954d7371
marcustakeyasu <116593545+marcustakeyasu@users.noreply.github.com>
2023-11-18 01:35:41 +0000

    Add files via upload

---
+++ CalculadoraJunitTest.java
@@ -0,0 +1,52 @@
+/*package projeto_calc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import junit.framework.Assert;
+
+import org.junit.jupiter.api.Test;
+
+class CalculadoraJunitTest {
+	
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testeSoma() {
+		
+		Calculadora calc = new Calculadora();	
+		Soma soma = new Soma();
+		int resultSoma = calc.calcular(soma, 5, 3);
+		Assert.assertEquals(2, resultSoma);  
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testeSubtracao() {
+		
+		Calculadora calc = new Calculadora();	
+		Subtracao subtracao = new Subtracao();
+		int resultSub = calc.calcular(subtracao, 12, 4);
+		Assert.assertEquals(8, resultSub);  
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testeDivisao() {
+		
+		Calculadora calc = new Calculadora();	
+		Divisao divisao = new Divisao();
+		int resultDiv = calc.calcular(divisao, 45, 9);
+		Assert.assertEquals(5, resultDiv);  
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testeMulti() {
+		
+		Calculadora calc = new Calculadora();	
+		Multiplicacao multiplicacao = new Multiplicacao();
+		int resultMulti = calc.calcular(multiplicacao, 45, 9);
+		Assert.assertEquals(5, resultMulti);  
+	}
+
+}*/
